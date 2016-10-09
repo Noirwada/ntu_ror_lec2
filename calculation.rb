946ff8ceae0bde27ac1c4034a233467e4269b3a8
kueikuei <kueikuei@users.noreply.github.com>
2016-10-10 01:11:07 +0800

    Add files via upload
    by ASCII to judge
    successfully judge non nuns type like as A-Z a-z

--- calculation.rb
+++ calculation.rb
@@ -4,14 +4,20 @@
 require 'pry'
 
 #operator = ['1','2','3','4'];
-def judgement(a,b)
-	puts "請輸入第一個數字："
-	input = gets.chomp;
-	puts "請輸入第二個數字："
-	input2 = gets.chomp;
-
+def judgement()
 	#num or not
 	#transfer to ASCII
+	begin
+	    puts "請輸入第一個數字："
+		input = gets.chomp;
+		puts "請輸入第二個數字："
+		input2 = gets.chomp;
+		#puts (65<input.ord.to_i.to_int);
+		#!((47<input.ord.to_i.to_int) && (input.ord.to_i.to_int<58) || (47<input2.ord.to_i.to_int) && (input2.ord.to_i.to_int<58))
+ 	end while ((57<input.ord.to_i.to_int) && (input.ord.to_i.to_int<127))|| ((57<input2.ord.to_i.to_int) && (input2.ord.to_i.to_int<127))
+ 		puts "您輸入的第一個數字為："+input+"  第二個數字為："+input2;
+		calculation(input,input2);
+=begin
 	if(65<a.ord<123)
 		puts "a is alphabet, input nummbers please!!";
 		judgement();
@@ -24,31 +30,35 @@ def judgement(a,b)
 			#if it's all num, calculate!
 			puts "您輸入的第一個數字為："+a+"  第二個數字為："+b;
 			calculation();
+		end
+	end
+=end
 end
 
-def calculation()
-	input = input.to_f;
-	input2 = input2.to_f;
+def calculation(a,b)
+	a = a.to_f;
+	b = b.to_f;
 
 	puts "欲加總請按整數1；欲加總請按整數2；欲加總請按整數3；欲加總請按整數4";
 
 	case gets.chomp.to_i;
-	when 1 then return (input + input2);
-	when 2 then return (input - input2);
-	when 3 then return (input * input2);
-	when 4 then return (input / input2);
+	when 1 then return (a + b);
+	when 2 then return (a - b);
+	when 3 then return (a * b);
+	when 4 then return (a / b);
 	end
 end
 
 #main 
 string = "歡迎來到天才小計算機，請輸入兩個預計算的數字！！";
-puts string;
-puts "請輸入第一個數字："
-input = gets.chomp;
-puts "請輸入第二個數字："
-input2 = gets.chomp;
+
+#puts string;
+#puts "請輸入第一個數字："
+#input = gets.chomp;
+#puts "請輸入第二個數字："
+#input2 = gets.chomp;
+
 
 #judge by func
-result = judgement(input, input2);
-
+result = judgement();
 puts result;
